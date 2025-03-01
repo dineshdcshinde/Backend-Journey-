54655456e473acabdde225608cb627e9fdef7f9d
Dinesh_DC <dineshshinde81563@gmail.com>
2025-03-01 22:21:28 +0530

    Made some schema level validations or made data sanitization

--- DevTinder/src/app.js
+++ DevTinder/src/app.js
@@ -16,13 +16,17 @@ dbConnectionFun()
     console.log("Error occuring while connectiong to db: " + err)
   );
 
+// Create
+// singup - [post] create a new user
 app.post("/singup", async (req, res) => {
   try {
-    const { userName, email, password } = req.body;
+    const { username, email, password, gender, skills } = req.body;
     let userInfo = await userModel.create({
-      userName,
+      username,
       email,
       password,
+      gender,
+      skills,
     });
     res.send(userInfo);
   } catch (error) {
@@ -30,6 +34,25 @@ app.post("/singup", async (req, res) => {
   }
 });
 
+// Read
+// user - [get] find the user by email
+app.get("/user", async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const getUser = await userModel.find({ email });
+
+    getUser.length > 0
+      ? res.send(getUser)
+      : res.status(400).json({ message: "User not found" });
+  } catch (err) {
+    console.log("Error occuring due to: " + err.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Read
+// feed - [get] give all the users
 app.get("/feed", async (req, res) => {
   try {
     const users = await userModel.find();
@@ -37,5 +60,56 @@ app.get("/feed", async (req, res) => {
     res.send(users);
   } catch (err) {
     console.log("Error occuring due to: " + err.message);
+  }
+});
+
+// Update
+// userupdate - [patch] it will update the specific part
+app.patch("/userupdate", async (req, res) => {
+  try {
+    const { email, username, password, gender, skills } = req.body;
+
+    if (password.length < 5 || username.length < 3) {
+      return res
+        .status(400)
+        .json({ message: "Password & username should be more than 5 chars" });
+    }
+      const updatedUser = await userModel.findOneAndUpdate(
+        { email },
+        {
+          username,
+          password,
+          gender,
+          skills,
+        }
+      );
+ 
+
+ 
+
+    updatedUser
+      ? res.send("User Updated")
+      : res.status(404).json({ message: "User not found" });
+  } catch (error) {
+    console.log("Error occuring due to: " + error.message);
+    res.status(500).json({ message: "Internal Server Error" + error.message });
+  }
+});
+
+// Delete
+// user - [delete] it will delete
+
+app.delete("/user", async (req, res) => {
+  try {
+    const { email } = req.body;
+    const deleteUser = await userModel.findOneAndDelete({ email });
+    // It is very important that to wrap the data with the {}
+
+    deleteUser
+      ? res.send("User Deleted")
+      : res.status(404).json({ message: "User not Deleted" });
+  } catch (err) {
+    console.log("Error occuring due to: " + err.message);
+    res.status(500).send("Internal Server Error");
   }
 });
--- DevTinder/src/models/user.model.js
+++ DevTinder/src/models/user.model.js
@@ -1,19 +1,35 @@
 const mongoose = require("mongoose");
 
-
 const userSchema = new mongoose.Schema({
-  userName: {
+  username: {
     type: String,
+    minLength: [5, "required 5 characters"],
     required: true,
-  },
-  email: {
-    type: String,
-    required: true,
+    trim: true,
   },
   password: {
     type: String,
+    trim: true,
+    minLength: [5, "required 5 characters"]
+  },
+  email: {
+    type: String,
+    unique: true, 
+    lowercase: true,
+    match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+    required: true,
+    trim: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "others"],
+    default: "male",
+  },
+  skills: {
+    type: [String],
   },
 });
+
 
 const userModel = mongoose.model("User", userSchema);
 
