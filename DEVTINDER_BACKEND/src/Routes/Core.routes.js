0f3aac93003af75d66392071b2ec8ccdfa8795eb
Dinesh_DC <dineshshinde81563@gmail.com>
2025-04-08 23:56:02 +0530

    Added the heart of the application feed api

--- DEVTINDER_BACKEND/src/Routes/Core.routes.js
+++ DEVTINDER_BACKEND/src/Routes/Core.routes.js
@@ -4,44 +4,48 @@ const ConnectionModel = require("../models/connection.model");
 const userModel = require("../models/user.model");
 const CoreRouter = express.Router();
 
+// DATA_PUBLIC_PROFILE
+const DATA_PUBLIC_PROFILE = ["name", "username", "location", "profilePicture"];
+
 // Get the connections
 CoreRouter.get("/feed", userauth, async (req, res) => {
   try {
-
     const loggedInUser = req.user;
-    // find all the connections and hide them
 
+    // get the user's collections so that we can avoid them to come in the feed
+    const connections = await ConnectionModel.find({
+      $or: [
+        {
+          fromUserId: loggedInUser._id,
+        },
+        {
+          toUserId: loggedInUser._id,
+        },
+      ],
+    }).select("fromUserId toUserId");
 
-    const connectionRequests = await ConnectionModel.find({
-      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
-    }).select("fromUserId  toUserId");
-
+    // we will create a alghorithm with the "new Set()" that will only add the unique elements
     const hideUsersFromFeed = new Set();
 
-    connectionRequests.forEach((req) => {
+    // we added the each req into the hideUsersFromFeed
+    connections.map((req) => {
       hideUsersFromFeed.add(req.fromUserId.toString());
       hideUsersFromFeed.add(req.toUserId.toString());
     });
 
-    const users = await userModel.find({
-      $and: [
-        { _id: { $nin: Array.from(hideUsersFromFeed) } },
-        { _id: { $ne: loggedInUser._id } },
-      ],
-    })
+    // find the connections in which this entries should not match
+    const feed = await userModel
+      .find({
+        $and: [
+          { _id: { $nin: Array.from(hideUsersFromFeed) } },
+          { _id: { $ne: loggedInUser._id } },
+        ],
+      })
+      .select(DATA_PUBLIC_PROFILE);
 
-    res.json({ data: users });
-
-
-
-
-    /* 
-        can't see self
-        can't see where the request is send or received 
-        can't see the user with the req ignored 
-     */
+    res.send(feed);
   } catch (err) {
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Internal server error", err: err.message });
   }
 });
 
